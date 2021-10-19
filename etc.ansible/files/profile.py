c32b12a494f725deb4c7b442c905a61c7303ca4b
ibre5041 <root@kicklinux.prod.vmware.haf>
2021-10-19 20:18:13 +0000

    Future bash_profile + Python helper

--- etc.ansible/files/profile.py
+++ etc.ansible/files/profile.py
@@ -14,7 +14,6 @@ import cx_Oracle
 class database():
     
     def __init__(self):
-        self.ora_status = 'Down'
         try:
             connection = cx_Oracle.connect(user='/', mode=cx_Oracle.SYSDBA)
             self._cursor = connection.cursor()
@@ -25,6 +24,8 @@ class database():
 
     def fetch_single_value(self, sql):
         try:
+            if not self._cursor:
+                return None
             self._cursor.execute(sql)
             (retval,) = self._cursor.fetchone()
             return retval
@@ -32,23 +33,14 @@ class database():
             #sys.stderr.write("{} failed with: {}".format(sql, str(e)))
             #raise BaseException("{} failed with: {}".format(sql, str(e)))
             return ""
-        except:
-            return ""
         
     def instance(self):
-        try:
-            self._cursor.execute(
-                """ SELECT
-                floor (sysdate - startup_time),
-                trunc (24 * ((sysdate - startup_time) - trunc (sysdate - startup_time))),
-                mod (trunc (1440 * ((sysdate - startup_time) - trunc (sysdate - startup_time))), 60),
-                mod (trunc (86400 * ((sysdate - startup_time) - trunc (sysdate - startup_time))), 60), status
-                FROM v$instance """)
-            (self.uptime_d, self.uptime_h, self.uptime_m, self.uptime_s, self.ora_status,) = self._cursor.fetchone()
-        except:
-            (self.uptime_d, self.uptime_h, self.uptime_m, self.uptime_s, self.ora_status,) = (None, None, None, None, None)
+        uptime = self.fetch_single_value(""" select NUMTODSINTERVAL(sysdate - startup_time, 'day') from v$instance """)
+        self.ora_uptime = str(uptime) if uptime else None
+        status = self.fetch_single_value(""" select status from v$instance """)
+        self.ora_status = status if status else 'DOWN'
             
-        if self.ora_status not in ['STARTED', 'MOUNTED']:
+        if self.ora_status not in ['STARTED', 'MOUNTED', 'DOWN']:
             try:
                 self._cursor.execute(""" SELECT database_role, dbid FROM v$database """)
                 (self.ora_dg_role, self.ora_dbid,) = self._cursor.fetchone()
@@ -65,18 +57,8 @@ class database():
         self.ora_version = self.fetch_single_value(
             """ SELECT version from v$instance """)
         
-        # Alert log dest
-        diag_dest = self.fetch_single_value(
-            """ SELECT max(value) as diag_dest FROM v$parameter WHERE name = 'diagnostic_dest' """)
-        if diag_dest:
-            self.alert_log = self.fetch_single_value(
-                """ SELECT REPLACE(value,'cdump','trace') as alert_log from v$parameter where name ='core_dump_dest' """)
-        else:
-            self.alert_log = self.fetch_single_value(
-                """ SELECT value as alert_log FROM v$parameter WHERE name = 'background_dump_dest' """)
-
         # This will fail on ASM, DBA_* views are nor accessible
-        try:
+        if self.ora_status not in ['STARTED', 'MOUNTED', 'DOWN']:
             if self.ora_version.startswith('12'):
                 self.ora_version = self.fetch_single_value(
                     """ select MIN(BUNDLE_ID) KEEP (DENSE_RANK LAST ORDER BY ACTION_TIME) BUNDLE_ID 
@@ -92,8 +74,16 @@ class database():
                     where TARGET_VERSION in 
                     (SELECT max(TARGET_VERSION) KEEP (DENSE_RANK LAST ORDER BY ACTION_TIME) as VER 
                     FROM dba_registry_sqlpatch where status='SUCCESS' and FLAGS not like '%J%') and ACTION = 'APPLY' """)
-        except BaseException as a:
-            pass
+
+        # Alert log dest
+        diag_dest = self.fetch_single_value(
+            """ SELECT max(value) as diag_dest FROM v$parameter WHERE name = 'diagnostic_dest' """)
+        if diag_dest:
+            self.ora_alert_log = self.fetch_single_value(
+                """ SELECT REPLACE(value,'cdump','trace') as alert_log from v$parameter where name ='core_dump_dest' """)
+        else:
+            self.ora_alert_log = self.fetch_single_value(
+                """ SELECT value as alert_log FROM v$parameter WHERE name = 'background_dump_dest' """)
 
         self.ora_dg_on = self.fetch_single_value(
             """ SELECT count(*) as ora_dg_on FROM v$archive_dest WHERE status = 'VALID' AND target = 'STANDBY' """)
@@ -106,10 +96,10 @@ class database():
         diag_dest = self.fetch_single_value(
             """ SELECT max(value) as diag_dest FROM v$parameter WHERE name = 'diagnostic_dest' """)
         if diag_dest:
-            self.alert_log = self.fetch_single_value(
+            self.ora_alert_log = self.fetch_single_value(
                 """ SELECT REPLACE(value,'cdump','trace') as alert_log from v$parameter where name ='core_dump_dest' """)
         else:
-            self.alert_log = self.fetch_single_value(
+            self.ora_alert_log = self.fetch_single_value(
                 """ SELECT value as alert_log FROM v$parameter WHERE name = 'background_dump_dest' """)
 
         self.ora_sgapga = self.fetch_single_value(
@@ -138,11 +128,12 @@ class homes():
         self.oracle_standalone = False
         self.oracle_install_type = None
         self.crs_home = None
-        self.homes = []
+        self.homes = {}
         self.ora_inventory = None
         self.orabase = None
         self.crsctl = None
         
+        # Check whether CRS/HAS is installed
         try:
             with open('/etc/oracle/ocr.loc') as f:
                 for line in f:
@@ -155,6 +146,7 @@ class homes():
         except:
             pass
 
+        # Try to detect CRS_HOME
         try:
             with open('/etc/oracle/olr.loc') as f:
                 for line in f:
@@ -168,6 +160,7 @@ class homes():
         except:
             pass
 
+        # Try to parse inventory.xml file to get list of ORACLE_HOMEs
         try:
             with open('/etc/oraInst.loc') as f:
                 for line in f:
@@ -179,7 +172,8 @@ class homes():
             inv_tree = minidom.parse(os.path.join(self.ora_inventory, 'ContentsXML', 'inventory.xml'))
             homes = inv_tree.getElementsByTagName('HOME')
             for home in homes:
-                self.homes.append(home.attributes['LOC'].value)
+                # TODO: skip for deleted ORACLE_HOME
+                self.add_home(home.attributes['LOC'].value)
         except:
             pass
 
@@ -194,18 +188,8 @@ class homes():
                 if line.startswith('#'):
                     continue
 
-                SID, ORACLE_HOME, _ = line.split(':')
-                if self.running_only:
-                    if SID in self.facts_item:
-                        self.facts_item[SID]['ORACLE_HOME'] = ORACLE_HOME
-                    else:
-                        #logging.warn('ORACLE_SID: {} is down'.format(SID))
-                        pass
-                else:
-                    if SID in self.facts_item:
-                        self.facts_item[SID]['ORACLE_HOME'] = ORACLE_HOME
-                    else:
-                        self.facts_item[SID] = {'SID': SID, 'ORACLE_HOME': ORACLE_HOME, 'running': False}
+                ORACLE_SID, ORACLE_HOME, _ = line.split(':')
+                self.add_sid(ORACLE_SID=ORACLE_SID, ORACLE_HOME=ORACLE_HOME)
 
 
     def list_processes(self):
@@ -228,7 +212,7 @@ class homes():
                     cmd_line = x.read().rstrip("\x00")
                     if not cmd_line.startswith('ora_pmon_') and not cmd_line.startswith('asm_pmon_'):
                         continue
-                    _, _, SID = cmd_line.split('_')
+                    _, _, ORACLE_SID = cmd_line.split('_')
 
                     piddir = os.path.dirname(cmd_line_file)
                     exefile = os.path.join(piddir, 'exe')
@@ -243,19 +227,18 @@ class homes():
                         # In case oracle binary is suid, ptrace does not work, 
                         # stat/readlink /proc/<pid>/exec does not work
                         # fails with: Permission denied
-                        # Then try to query the same information from CRS
+                        # Then try to query the same information from CRS (if possible)
                         ORACLE_HOME = None
 
                         if self.crsctl:
-                            dfilter = '((TYPE = ora.database.type) and (GEN_USR_ORA_INST_NAME = {ORACLE_SID}))'.format(ORACLE_SID=SID)
+                            dfilter = '((TYPE = ora.database.type) and (GEN_USR_ORA_INST_NAME = {}))'.format(ORACLE_SID)
                             proc = subprocess.Popen([self.crsctl, 'stat', 'res', '-p', '-w', dfilter], stdout=subprocess.PIPE)
                             for line in iter(proc.stdout.readline,''):
                                 if line.startswith('ORACLE_HOME='):
                                     (_, ORACLE_HOME,) = line.strip().split('=')
                         pass
                     
-                    ORACLE_BASE = self.base_from_home(ORACLE_HOME)
-                    self.facts_item[SID]= {'SID': SID, 'ORACLE_HOME': ORACLE_HOME, 'running': True, 'ORACLE_BASE': ORACLE_BASE}
+                    self.add_sid(ORACLE_SID=ORACLE_SID, ORACLE_HOME=ORACLE_HOME, running=True)
 
             #except FileNotFoundError: # Python3
             except EnvironmentError as e:
@@ -277,9 +260,9 @@ class homes():
                 if line.startswith('ORACLE_HOME='):
                     (_, ORACLE_HOME,) = line.strip().split('=')
                 if ORACLE_SID and ORACLE_HOME:
-                    ORACLE_BASE = self.base_from_home(ORACLE_HOME)
-                    self.facts_item[ORACLE_SID] = {'SID': ORACLE_SID, 'ORACLE_HOME': ORACLE_HOME, 'running': None, 'ORACLE_BASE': ORACLE_BASE}
+                    self.add_sid(ORACLE_SID=ORACLE_SID, ORACLE_HOME=ORACLE_HOME)
                     (ORACLE_HOME, ORACLE_SID) = (None, None)
+
 
     def base_from_home(self, ORACLE_HOME):
         """ execute $ORACLE_HOME/bin/orabase to get ORACLE_BASE """
@@ -291,6 +274,29 @@ class homes():
                 if line.strip():
                     ORACLE_BASE = line.strip()
         return ORACLE_BASE
+    
+
+    def add_home(self, ORACLE_HOME):
+        if ORACLE_HOME not in self.homes:
+            ORACLE_BASE = self.base_from_home(ORACLE_HOME)
+            self.homes[ORACLE_HOME] = {'ORACLE_HOME': ORACLE_HOME, 'ORACLE_BASE': ORACLE_BASE}
+
+    
+    def add_sid(self, ORACLE_SID, ORACLE_HOME = None, running = False):
+        if ORACLE_SID in self.facts_item:
+            sid = self.facts_item[ORACLE_SID]
+            if ORACLE_HOME:
+                sid['ORACLE_HOME'] = ORACLE_HOME
+            if running:
+                sid['running'] = running
+            if running is not None and sid['running'] is not True:
+                sid['running'] = running
+        else:
+            self.add_home(ORACLE_HOME)
+            self.facts_item[ORACLE_SID] = {'ORACLE_SID': ORACLE_SID
+                                           , 'ORACLE_HOME': ORACLE_HOME
+                                           , 'ORACLE_BASE': self.homes[ORACLE_HOME]['ORACLE_BASE']
+                                           , 'running': running}
 
 
 if __name__ == '__main__':
@@ -317,8 +323,8 @@ if __name__ == '__main__':
             print(json.dumps(h.facts_item, indent=4))
         else:
             for k in sorted(h.facts_item.keys()):
-                print("{SID}\t{ORACLE_HOME}\t{ORACLE_BASE}"
-                      .format(SID=k
+                print("{ORACLE_SID}\t{ORACLE_HOME}\t{ORACLE_BASE}"
+                      .format(ORACLE_SID=k
                               , ORACLE_HOME=h.facts_item[k]['ORACLE_HOME']
                               , ORACLE_BASE=h.facts_item[k]['ORACLE_BASE']))
 
